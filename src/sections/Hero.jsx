42383880b3e48e24ed97522b20c07f1d1c69276e
Chanawin-kmpn <pond_chanawin@hotmail.com>
2025-06-23 13:36:31 +0700

    first commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { words } from "../../constants";
+import Button from "../components/Button";
+
+const Hero = () => {
+	return (
+		<section id="hero" className="relative overflow-hidden">
+			<div className="absolute top-0 left-0 z-10">
+				<img src="/images/bg.png" alt="background" />
+			</div>
+
+			<div className="hero-layout">
+				{/* Left: Hero content */}
+				<header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+					<div className="flex flex-col gap-7">
+						<div className="hero-text">
+							<h1>
+								Shaping
+								<span className="slide">
+									<span className="wrapper">
+										{words.map((word) => (
+											<span
+												className="flex items-center md:gap-3 gap1 pb-2"
+												key={word.text}
+											>
+												<img
+													src={word.imgPath}
+													alt={word.text}
+													className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+												/>
+
+												<span>{word.text}</span>
+											</span>
+										))}
+									</span>
+								</span>
+							</h1>
+							<h1>into Real Projects</h1>
+							<h1>that Deliver Results</h1>
+						</div>
+						<p className="text-white-50 md:text-xl relativve z-10 pointer-events-none">
+							Hi, I'm Chanawin, a developer based in Thailand with a passion for
+							code.
+						</p>
+						<Button
+							className="md:w-80 md:h-15 w-60 h-12"
+							id="button"
+							text="See my Work"
+						/>
+					</div>
+				</header>
+				{/* Right: 3D model */}
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
